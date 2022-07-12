3a15aa4302d6ec6502945ae7859c4add04a49b34
steventid <108754105+steventid@users.noreply.github.com>
2022-07-12 18:24:15 -0500

    contact updates, new navigation

--- filters.js
+++ filters.js
@@ -1,35 +1,48 @@
 //link buttons on page load
 $(document).ready(function(){
-  $("#resume").click(function(){filter("resume")});
-  $("#all").click(function(){filter("spotlight")});
-  $("#godot").click(function(){filter("godot")});
-  $("#vb6").click(function(){filter("vb6")});
-  $("#cpp").click(function(){filter("cpp")});
+  $('#resume').click(function(){filter($(this).attr('id'));});
+  $('#spotlight').click(function(){filter($(this).attr('id'));});
+  $('#godot').click(function(){filter($(this).attr('id'));});
+  $('#vb6').click(function(){filter($(this).attr('id'));});
+  $('#cpp').click(function(){filter($(this).attr('id'));});
   
   //organize project list when page loads to prettify left/right ordering
-  filter("spotlight", false);
+  filter(sessionStorage.getItem('filterName') ?? 'spotlight');
 });
 
-function filter(type, scroll=true) {
+//dynamically resize contact form to fit scaling
+window.addEventListener('message', function(e) {
+  let message = e.data;
+  document.querySelector('#contact-frame').style.height = message.height + 'px';
+}, false);
+
+function filter(type='spotlight') {
   //hide all spotlights, show selected type
   $('.spotlight').hide();
   $('.'+type).show();
+  
+  //save the filter type to the session
+  sessionStorage.setItem("filterName", type);
+  
   //dynamically make it alternate between left and right based on visibility
   $('.spotlight:visible').each(function(i) {
     //remove the side formatting
     $(this).removeClass("orient-right orient-left");
-    
+    //alternate which side text/images are on
     if (i % 2 == 0) {
       $(this).addClass("orient-right");
     } else {
       $(this).addClass("orient-left");
     }
-    
-    //update 7/11/22 - smooth-scroll class from css already does this
-    //scroll to content, include .stop(true, false) to keep animation from "sticking" once the scroll stops
-    //if (scroll == true) {
-    //$('html, body').stop(true, false).animate({
-    //  scrollTop: $("#content-start").offset().top});
-    //} //end if scroll == true
+  });
+  
+  //set whether or not button is clicked
+  $('.filter').each(function() {
+    //deselect all
+    $(this).removeClass('rubix_active');
+    //select current filter
+    if ($(this).attr('id') == type) {
+      $(this).addClass('rubix_active');
+    }
   });
 }
