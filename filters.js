733cebbad2ddef7a5bc80971ee7b44a7a6ae7981
Steven Tidwell <steventid1313@gmail.com>
2023-05-31 13:58:50 -0500

    Added Uber-Heck

--- filters.js
+++ filters.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
   $('#vb6').click(function(){filter($(this).attr('id'));});
   $('#cpp').click(function(){filter($(this).attr('id'));});
   $('#bash').click(function(){filter($(this).attr('id'));});
+  $('#javascript').click(function(){filter($(this).attr('id'));});
   
   //organize project list when page loads to prettify left/right ordering
   filter(sessionStorage.getItem('filterName') ?? 'spotlight');
